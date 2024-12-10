325b46dad3a7ab8c82a9b3e9353b229ff541499b
ahmaddynugroho <ahmaddwi700@gmail.com>
2024-12-10 21:32:44 +0700

    chore: add nuxt icon module

--- app.vue
+++ app.vue
@@ -4,10 +4,13 @@
     <h1 class="text-3xl font-bold underline text-primary-400">tailwind test</h1>
     <Button label="Verify" />
     <Button label="Verify Tailwind" class="bg-primary-400" />
+    <Button label="Verify Icon">
+      <template #icon>
+        <Icon name="uil:github" style="color: black" />
+      </template>
+    </Button>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
-    1. primevue
-    2. tailwind
-    3. setup noir
+    1. primevue 2. tailwind 3. setup noir
   </div>
 </template>
