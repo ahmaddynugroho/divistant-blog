1baf452a966a39a5ecd2230da8ac5e8c77ae2ff2
ahmaddynugroho <ahmaddwi700@gmail.com>
2024-12-10 21:21:46 +0700

    chore: add primevue

--- app.vue
+++ app.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
+    <h1 class="text-3xl font-bold underline">tailwind test</h1>
+    <h1 class="text-3xl font-bold underline text-primary-400">tailwind test</h1>
+    <Button label="Verify" />
+    <Button label="Verify Tailwind" class="bg-primary-400" />
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
+    1. primevue
+    2. tailwind
+    3. setup noir
   </div>
 </template>
