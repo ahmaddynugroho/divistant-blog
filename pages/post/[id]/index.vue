6ff8a0ff45f50a94d8b146b4888f024693766d85
ahmaddynugroho <ahmaddwi700@gmail.com>
2024-12-12 05:29:10 +0700

    refactor: add navbar to post detail

--- pages/post/[id]/index.vue
+++ pages/post/[id]/index.vue
@@ -8,6 +8,7 @@ post.value = posts.detail(Number(route.params.id))
 
 <template>
   <div class="m-4">
+    <NavBar class="mb-4" />
     <h1 class="text-3xl">{{ post.title }}</h1>
     <p class="text-sm text-primary-400">{{ post.tags.join(', ') }}</p>
     <p class="text-primary-100">{{ post.body }}</p>
